a6826e2d2e365688efeade41c5819c1199d57b5d
nymphe <nymphe@nymphe-GB-BRi5-H-8250.openair4G.eur>
2020-07-21 17:03:12 +0200

    request

---
+++ src/app/app_firas.h
@@ -0,0 +1,32 @@
+
+
+#ifndef APP_FIRAS_H_
+#define APP_FIRAS_H_
+
+#include "component.h"
+#include "rib_common.h"
+
+namespace flexran {
+
+  namespace app {
+
+    namespace management {
+
+      class app_firas : public component {
+
+      public:
+
+        app_firas(const rib::Rib& rib, const core::requests_manager& rm,
+            event::subscription& sub);
+        void tick(uint64_t ms);
+
+      private:
+        std::set<uint64_t> inactive_bs_;
+
+      
+      };
+    }
+  }
+}
+
+#endif /* APP_FIRAS_H_ */
