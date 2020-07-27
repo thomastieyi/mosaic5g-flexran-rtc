cf7d9df95140a554b6658304ae0bdf97cdc98e5c
nymphe <nymphe@nymphe-GB-BRi5-H-8250.openair4G.eur>
2020-07-27 11:29:22 +0200

    verison 1

--- src/app/app_firas.cc
+++ src/app/app_firas.cc
@@ -8,6 +8,7 @@
 #include <thread>
 #include <curl/curl.h>
 #include <regex>
+#include <iostream>
 
 /*flexran::app::log::app_firas::app_firas(const rib::Rib& rib,
     const core::requests_manager& rm, event::subscription& sub)
@@ -30,6 +31,51 @@ flexran::app::log::app_firas::~app_firas()
   curl_global_cleanup();
 }
 */
+
+
+
+#define BUFSIZE 65536*2
+int cb_call = 0;
+char buf[BUFSIZE];
+size_t bufpos = 0;
+
+size_t callback(char *p, size_t , size_t nmemb, void *v) {
+  if (nmemb + bufpos + 1 > BUFSIZE) {
+    nmemb = BUFSIZE - bufpos - 1;
+    std::cerr << "buffer size exceeded, limiting output to " << nmemb << "\n";
+  }
+
+  std::memcpy(&buf[bufpos], p, nmemb);
+  bufpos += nmemb;
+  buf[bufpos] = 0;
+
+  cb_call++;
+  std::cout << __func__ << ":" << cb_call << ": writing " << nmemb
+            << "B, total " << bufpos << "B\n";
+  	
+  return nmemb; // if buffer exceeded, reduced nmemb will trigger error in libcurl
+}
+
+void number_output() {
+  std::vector<std::string> f;
+  std::string s{buf};
+  std::string delimiter = "\n";
+
+  size_t pos = 0;
+  while ((pos = s.find(delimiter)) != std::string::npos) {
+    f.push_back(s.substr(0, pos));
+    s.erase(0, pos + delimiter.length());
+  }
+
+  int i = 0;
+  for (const std::string& si : f) {
+    std::cout << i << ": " << si << "\n";
+    i++;
+  }
+}
+ 
+
+
 
 
 flexran::app::management::app_firas::app_firas(const flexran::rib::Rib& rib,
@@ -73,40 +119,30 @@ void flexran::app::management::app_firas::tick(uint64_t ms)
 
  
 
-
 CURL *flexran::app::management::app_firas::curl_create_transfer(const std::string& addr/*,const std::string& data*/)
 {
-
-  CURL *curl;
+CURL *curl;
   CURLcode res;
- 
   curl = curl_easy_init();
-  if(curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, addr.c_str());
-    //curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, data.length());
-    curl_easy_setopt(curl, CURLOPT_POST, 0L);
-    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
-   /* provide lambda that swallows all output. operator+ converts to function
-    * pointer which is necessary since we call a C library */
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION,
-      +[](char *, size_t, size_t nmemb, void *) { return nmemb; });
-	
-
-
-
-    /* example.com is redirected, so we tell libcurl to follow redirection */ 
-    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
- 
-    /* Perform the request, res will get the return code */ 
-    res = curl_easy_perform(curl);
-    /* Check for errors */ 
-    if(res != CURLE_OK)
-      fprintf(stderr, "curl_easy_perform() failed: %s\n",
-              curl_easy_strerror(res));
- 
-    /* always cleanup */ 
-    curl_easy_cleanup(curl);
+  if (!curl) {
+    std::cerr << "curl_easy_init() failed\n";
+    return 0;
   }
-   return curl;
+  curl_easy_setopt(curl, CURLOPT_URL, addr.c_str());
+  //curl_easy_setopt(curl, CURLOPT_VERBOSE, 0);
+  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
+  /* Perform the request, res will get the return code */
+  res = curl_easy_perform(curl);
+  	
+  /* Check for errors */
+ number_output();	
+  if (res != CURLE_OK)
+    std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << "\n";
+  else
+    std::cout << "curl_easy_perform() finished\n";
+  number_output();
+  /* always cleanup */
+  curl_easy_cleanup(curl);
+  return 0;
 
 }
