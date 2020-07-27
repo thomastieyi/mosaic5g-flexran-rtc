5e1556cea46746ca26e0dc02e24ccdfa1fbf3cab
nymphe <nymphe@nymphe-GB-BRi5-H-8250.openair4G.eur>
2020-07-27 13:59:38 +0200

    version 3 easy

--- src/app/app_firas.cc
+++ src/app/app_firas.cc
@@ -10,27 +10,6 @@
 #include <regex>
 #include <iostream>
 
-/*flexran::app::log::app_firas::app_firas(const rib::Rib& rib,
-    const core::requests_manager& rm, event::subscription& sub)
-  : component(rib, rm, sub),
-    active_since_(std::chrono::system_clock::now()),
-    sent_packets_(0),
-    freq_stats_ (100),
-    freq_config_(5000),
-    batch_stats_max_no_(100),
-    batch_config_max_no_(5)
-{
-  app_firas_ep_.push_back("localhost:9200"),
-  curl_global_init(CURL_GLOBAL_DEFAULT);
-  curl_multi_ = curl_multi_init();
-}
-flexran::app::log::app_firas::~app_firas()
-{
-  disable_logging();
-  curl_multi_cleanup(curl_multi_);
-  curl_global_cleanup();
-}
-*/
 
 
 
@@ -41,11 +20,12 @@ size_t bufpos = 0;
 
 size_t callback(char *p, size_t , size_t nmemb, void *v) {
   if (nmemb + bufpos + 1 > BUFSIZE) {
+	
     nmemb = BUFSIZE - bufpos - 1;
     std::cerr << "buffer size exceeded, limiting output to " << nmemb << "\n";
   }
 
-  std::memcpy(&buf[0], p, nmemb);
+  std::memcpy(&buf[bufpos], p, nmemb);
   bufpos += nmemb;
   buf[bufpos] = 0;
 
@@ -72,6 +52,7 @@ void number_output() {
     std::cout << i << ": " << si << "\n";
     i++;
   }
+bufpos=0;
 }
  
 
@@ -119,7 +100,7 @@ void flexran::app::management::app_firas::tick(uint64_t ms)
 
  
 
-CURL *flexran::app::management::app_firas::curl_create_transfer(const std::string& addr/*,const std::string& data*/)
+CURL *flexran::app::management::app_firas::curl_create_transfer(const std::string& addr)
 {
 CURL *curl;
   CURLcode res;
@@ -129,13 +110,11 @@ CURL *curl;
     return 0;
   }
   curl_easy_setopt(curl, CURLOPT_URL, addr.c_str());
-  //curl_easy_setopt(curl, CURLOPT_VERBOSE, 0);
   curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
   /* Perform the request, res will get the return code */
   res = curl_easy_perform(curl);
   	
-  /* Check for errors */
- number_output();	
+  /* Check for errors */	
   if (res != CURLE_OK)
     std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << "\n";
   else
